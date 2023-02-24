fe01e9c1df0e8885800add306b5fb236e2981d72
Minyaraw <120593004+Minyaraw@users.noreply.github.com>
2023-02-24 00:46:00 -0500

    updates

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 
 
 
-
-
-
-
-
-
+let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
-    console.log("The button was clicked")
+    count = count + 1
+    countEl.innerText = count
+
 }
